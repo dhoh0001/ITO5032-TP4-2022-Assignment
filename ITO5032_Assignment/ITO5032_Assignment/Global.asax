0a1fded767139ab0de2d400e1713ef773e17f240
Dan <dhoh001@student.monash.edu>
2022-07-26 22:31:26 +1000

    Initial project creation - ASP.NET Web Application - .NET framework - MVC - C# - Individual User Account

---
+++ ITO5032_Assignment/ITO5032_Assignment/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ITO5032_Assignment.MvcApplication" Language="C#" %>
---
+++ ITO5032_Assignment/ITO5032_Assignment/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ITO5032_Assignment.Startup))]
+namespace ITO5032_Assignment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
