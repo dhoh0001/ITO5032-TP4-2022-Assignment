8c42cd0a06a086e19250099b5575243b12942f3d
Dan <dhoh001@student.monash.edu>
2022-07-28 23:00:57 +1000

    Adding File Upload

---
+++ ITO5032_Assignment/ITO5032_Assignment/Controllers/FileUploadController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ITO5032_Assignment.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        // GET: FileUpload
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult UploadFile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadFile(HttpPostedFileBase file)
+        {
+            try
+            {
+                if (file.ContentLength > 0)
+                {
+                    string _FileName = Path.GetFileName(file.FileName);
+                    string _path = Path.Combine(Server.MapPath("~/UploadedFiles"), _FileName);
+                    file.SaveAs(_path);
+                }
+                ViewBag.Message = "File Uploaded Successfully!!";
+                return RedirectToAction("Index");
+
+            }
+            catch (Exception e)
+            {
+                ViewBag.Message = "File upload failed!!";
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
---
+++ ITO5032_Assignment/ITO5032_Assignment/Models/FileUploadModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ITO5032_Assignment.Models
+{
+    public class FileUploadModel
+    {
+    }
+}
