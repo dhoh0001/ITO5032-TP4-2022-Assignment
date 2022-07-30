c69e3eb34fa6eb5264f9a7dbb517c8a898961b56
Dan <dhoh001@student.monash.edu>
2022-07-30 13:51:53 +1000

    Badge notifications working on ui, backedn to be built

---
+++ ITO5032_Assignment/ITO5032_Assignment/Controllers/NotificationController.cs
@@ -0,0 +1,18 @@
+﻿using ITO5032_Assignment.Models;
+using System;
+using System.Web.Mvc;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Threading.Tasks;
+
+namespace ITO5032_Assignment.Controllers
+{
+    public class NotificationController : Controller
+    {
+        public ActionResult test()
+        {
+            ViewData["i"] = 9;
+            return PartialView("_BadgePartial");
+        }
+    }
+}
---
+++ ITO5032_Assignment/ITO5032_Assignment/Models/NotificationModels.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ITO5032_Assignment.Models
+{
+    public class NotificationModels
+    {
+        public String message { get; set; }
+        public int id { get; set; }
+    }
+}
