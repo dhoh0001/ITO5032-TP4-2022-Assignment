0fdd8d7711c0faee8f4911cd37937b5b9589bd45
Dan <dhoh001@student.monash.edu>
2022-08-16 21:13:07 +1000

    commit

--- ITO5032_Assignment/SQLQuery2.sql
+++ ITO5032_Assignment/SQLQuery2.sql
@@ -1,7 +1,1 @@
-﻿select * from AppUsers;
-select * from AspNetUsers;
-select * from Bookings;
-select * from Bookables;
-select * from locations;
-select * from files;
-update bookables set service_provider_id = 16
+﻿select * from AppUsers
